cb3aa3df1da974f36d9af4b2a0b2b267af147e10
Eduardo Carranza <eduardocarranza0812@gmail.com>
2021-09-19 15:54:50 -0400

    Creates the AddItem and updateSubtotal methods

--- js/function.js
+++ js/function.js
@@ -1,6 +1,60 @@
 //Author: Eduardo Carranza
 
 
+function addItem(){
+  var itemTitle = $('#item-title').val();
+  var itemPrice = $('#item-price').val();
+
+  var itemBoard = $('#item-table');
+  var itemObject = $('<div></div>' , {
+    "class": 'col-12 my-3 fw-bold',
+    html: "<div class='row'>" + 
+
+          "<div class='col'>" +
+          "<div class='text-white text-shadow'>" + itemTitle + "</div>" +
+          "</div>" + 
+
+          "<div class='col'>" +
+          "<div class='text-white text-shadow'>" + "$" + itemPrice + "</div>" +
+          "</div>" +
+
+          "<div class='col'>" +
+          "<div>" +
+          "<label>" +
+          "<span class='text-white text-shadow'>" + "QTY" + "</span>" + "<input type='number' class='item-amount' />" +
+          "</label>" + 
+          "</div>" +
+          "</div>" +
+
+          "<div class='col'>" +
+          "<button type='button' class='btn btn-secondary'>" + "Remove" + "</button>" +
+          "</div>" +
+
+          "<div class='col'>" +
+          "<div class='text-white text-shadow'>" + "$--.--" + "</div>" +
+          "</div>" +
+
+          "</div>"
+  });
+
+  itemBoard.append(itemObject);
+  $('#item-title').val('');
+  $('#item-price').val('');
+
+}
+
+function updateSubtotal(){
+  var parent = $(this).parents('.col');
+  var itemPriceObject = parent.next().next();
+  var currentPrice = parseFloat(parent.prev().children().first().text().substring(1));
+  var itemAount = Number($(this).val());
+  var subtotal = parseFloat(currentPrice * itemAount);
+  itemPriceObject.children().first().text('$' + subtotal);
+}
+
 $(document).ready(function(){
  
+  $('#create-item').click(addItem);
+  $("input.item-amount").change(updateSubtotal);
+
 });
